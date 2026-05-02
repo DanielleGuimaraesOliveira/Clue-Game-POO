a8847323a2879e4608cad10f9942ff0c3baeeb71
Danielle Guimarães <danielleguimaraes@tecgraf.puc-rio.br>
2026-05-02 12:33:40 -0300

    adicionando jogadorTest

--- test-model/model/JogadorTest.java
+++ test-model/model/JogadorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class JogadorTest {
 
-    // 🟢 TESTE 1: jogador recebe carta
+    
     @Test
     void deveReceberCarta() {
         Jogador jogador = new Jogador("Ana", new PecaSuspeito("Srta. Scarlet"));
@@ -17,7 +17,7 @@ public class JogadorTest {
         assertEquals(1, jogador.getMao().size());
     }
 
-    // 🟢 TESTE 2: jogador mostra carta quando possui
+  
     @Test
     void deveMostrarCartaSePossuir() {
         Jogador jogador = new Jogador("Ana", new PecaSuspeito("Srta. Scarlet"));
@@ -31,7 +31,7 @@ public class JogadorTest {
         assertEquals("Faca", resultado.getNome());
     }
 
-    // 🟢 TESTE 3: retorna null se não tiver a carta
+
     @Test
     void naoDeveMostrarCartaSeNaoPossuir() {
         Jogador jogador = new Jogador("Ana", new PecaSuspeito("Srta. Scarlet"));
@@ -43,7 +43,7 @@ public class JogadorTest {
         assertNull(resultado);
     }
 
-    // 🟢 TESTE 4: jogador começa não eliminado
+
     @Test
     void jogadorNaoComecaEliminado() {
         Jogador jogador = new Jogador("Ana", new PecaSuspeito("Srta. Scarlet"));
