ad3fbbb89c1d913ad603ad7f0abfb5980d059fa5
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-05-12 18:28:00 +0000

    modificando o gerenciador para ele ser um singleton

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ class Main {
 
 	public static void main(String[] args) {
 		
-		GerenciadorDePartida jogo = new GerenciadorDePartida();
-
+		GerenciadorDePartida jogo = GerenciadorDePartida.getInstance();
+		
 		// Jogador j1 = new Jogador("Ana", new PecaSuspeito("Srta. Scarlet"));
 		// Jogador j2 = new Jogador("João", new PecaSuspeito("Sr. Verde"));
-
+		
 		jogo.adicionarJogador("Ana", "Srta. Scarlet");
 		jogo.adicionarJogador("João","Sr. Verde");
-
 		jogo.iniciarPartida();
+		
 		
 		System.out.println("Jogo iniciado!");
 		
