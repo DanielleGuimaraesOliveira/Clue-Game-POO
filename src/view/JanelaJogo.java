4169f94a72c916146048926ea93a3db49480950f
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-05-12 18:33:45 +0000

    adicionando a classe janelaJogo

---
+++ src/view/JanelaJogo.java
@@ -0,0 +1,15 @@
+package view;
+
+public class JanelaJogo extends javax.swing.JFrame {
+
+    public JanelaJogo() {
+        iniciaComponentes();
+    }
+
+    private void iniciaComponentes() {
+        setTitle("Clue Game");
+        setSize(1200, 800);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+    
+}
