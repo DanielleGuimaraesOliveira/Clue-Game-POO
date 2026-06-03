456bbdf429b81b95af32bd4c83dda8d275f866d2
Danielle Guimarães <danielleguimaraes@tecgraf.puc-rio.br>
2026-06-03 13:21:03 -0300

    [refactor] fazendo carta e casa implementa as interfaces e deixa-las package

--- src/model/Carta.java
+++ src/model/Carta.java
@@ -1,5 +1,9 @@
 package model;
-class Carta {
+
+import Interfaces.ICarta;
+
+// Classe Carta agora é package-private e implementa ICarta
+class Carta implements ICarta {
 	
 	private String nome;
 	private TipoCarta tipo;
