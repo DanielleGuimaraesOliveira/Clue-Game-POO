416455acadeb82074fd50aa6a8effa802595414e
Danielle Guimarães <danielleguimaraes@tecgraf.puc-rio.br>
2026-06-03 14:05:25 -0300

    [feat] adicionando logica de salva e carregar a partida e substituindo classes por interface

--- src/model/GerenciadorDeJogadores.java
+++ src/model/GerenciadorDeJogadores.java
@@ -42,6 +42,21 @@ public class GerenciadorDeJogadores {
     	jogadorAtual = jogadores.get((index + 1) % jogadores.size());
     }
 
+	public void definirJogadorAtual(Jogador jogador) {
+		if (jogadores.contains(jogador)) {
+			this.jogadorAtual = jogador;
+		}
+	}
+
+	public Jogador buscarJogadorPorNome(String nome) {
+		for (Jogador jogador : jogadores) {
+			if (jogador.getPersonagem().getNome().equals(nome)) {
+				return jogador;
+			}
+		}
+		return null;
+	}
+
     // get e set
     public Jogador getJogadorAtual() {
     	return jogadorAtual;
